22c435a03fa984f0c9fa6d2af746507590a34fa4
T-101 <antti.rummukainen@gmail.com>
2022-11-23 16:55:51 +0200

    prefetch sounds, order soundboards by name

--- soundboard/views.py
+++ soundboard/views.py
@@ -1,3 +1,4 @@
+from django.db.models.functions import Lower
 from django.views import generic
 
 from soundboard.models import Soundboard
@@ -5,8 +6,12 @@ from soundboard.models import Soundboard
 
 class SoundboardListView(generic.ListView):
     model = Soundboard
+    ordering = [Lower("name")]
 
 
 class SoundboardDetailView(generic.DetailView):
     model = Soundboard
     slug_field = "slug"
+
+    def get_queryset(self):
+        return self.model.objects.prefetch_related("sounds").all()
